e313e0203e955e6a2458ac253d71ba4487dad483
Alensh Maharjan <alensh.am12@gmail.com>
2017-09-03 11:58:27 +0545

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.alensh.customtoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void DisplayToast(View v){
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER,0,0);
+
+
+        LayoutInflater inflater = getLayoutInflater();
+        View appear =inflater.inflate(R.layout.toast_layout,
+                (ViewGroup)findViewById(R.id.toastCustomed));
+        toast.setView(appear);
+        toast.show();
+     }
+
+}
